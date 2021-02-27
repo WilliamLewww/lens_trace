666d94316b4dda93c975da7e6f24a1d93ad34d85
WilliamLewww <WilliamMilesLew@gmail.com>
2021-02-26 21:13:56 -0800

    added: build acceleration structure

--- src/acceleration_structure.cpp
+++ src/acceleration_structure.cpp
@@ -1,12 +1,5 @@
 #include "acceleration_structure.h"
 
-struct PrimitiveInfo {
-  uint64_t primitiveNumber;
-  float boundsMin[3];
-  float boundsMax[3];
-  float centroid[3];
-};
-
 AccelerationStructure::AccelerationStructure(Model* model) {
   tinyobj::attrib_t attrib = model->getAttrib();
   std::vector<tinyobj::shape_t> shapes = model->getShapes();
@@ -44,7 +37,11 @@ AccelerationStructure::AccelerationStructure(Model* model) {
     float centroidZ = 0.5 * boundsMinZ + 0.5 * boundsMaxZ;
 
     PrimitiveInfo primitiveInfo = {
-      .primitiveNumber = x,
+      .index = (int)x,
+      .vertexA = {primitiveList[x][0][0], primitiveList[x][1][0], primitiveList[x][2][0]},
+      .vertexB = {primitiveList[x][0][1], primitiveList[x][1][1], primitiveList[x][2][1]},
+      .vertexC = {primitiveList[x][0][2], primitiveList[x][1][2], primitiveList[x][2][2]},
+
       .boundsMin = {boundsMinX, boundsMinY, boundsMinZ},
       .boundsMax = {boundsMaxX, boundsMaxY, boundsMaxZ},
       .centroid = {centroidX, centroidY, centroidZ}
@@ -52,8 +49,99 @@ AccelerationStructure::AccelerationStructure(Model* model) {
 
     primitiveInfoList.push_back(primitiveInfo);
   }
+
+  int totalNodes = 0;
+  std::vector<PrimitiveInfo*> orderedPrimitiveList;
+  BVHBuildNode* root = recursiveBuild(primitiveInfoList, 0, primitiveList.size(), &totalNodes, orderedPrimitiveList);
 }
 
 AccelerationStructure::~AccelerationStructure() {
 
+}
+
+BVHBuildNode* AccelerationStructure::recursiveBuild(std::vector<PrimitiveInfo>& primitiveInfoList, int start, int end, int* totalNodes, std::vector<PrimitiveInfo*>& orderedPrimitiveList) {
+  BVHBuildNode* node = new BVHBuildNode();
+  (*totalNodes) += 1;
+
+  float boundsMin[3];
+  float boundsMax[3];
+  for (int x = start; x < end; x++) {
+    node->boundsMin[0] = std::min(boundsMin[0], primitiveInfoList[x].boundsMin[0]);
+    node->boundsMin[1] = std::min(boundsMin[1], primitiveInfoList[x].boundsMin[1]);
+    node->boundsMin[2] = std::min(boundsMin[2], primitiveInfoList[x].boundsMin[2]);
+
+    node->boundsMax[0] = std::max(boundsMax[0], primitiveInfoList[x].boundsMax[0]);
+    node->boundsMax[1] = std::max(boundsMax[1], primitiveInfoList[x].boundsMax[1]);
+    node->boundsMax[2] = std::max(boundsMax[2], primitiveInfoList[x].boundsMax[2]);
+  }
+
+  int primitiveCount = end - start;
+  if (primitiveCount == 1) {
+    int firstPrimitiveOffset = orderedPrimitiveList.size();
+    for (int x = start; x < end; x++) {
+      orderedPrimitiveList.push_back(&primitiveInfoList[x]);
+    }
+
+    node->leftChild = NULL;
+    node->rightChild = NULL;
+
+    node->splitAxis = -1;
+    node->firstPrimitiveOffset = firstPrimitiveOffset;
+    node->primitiveCount = primitiveCount;
+  }
+  else {
+    float centroidBoundsMin[3];
+    float centroidBoundsMax[3];
+    for (int x = start; x < end; x++) {
+      centroidBoundsMin[0] = std::min(centroidBoundsMin[0], primitiveInfoList[x].centroid[0]);
+      centroidBoundsMin[1] = std::min(centroidBoundsMin[1], primitiveInfoList[x].centroid[1]);
+      centroidBoundsMin[2] = std::min(centroidBoundsMin[2], primitiveInfoList[x].centroid[2]);
+
+      centroidBoundsMax[0] = std::max(centroidBoundsMax[0], primitiveInfoList[x].centroid[0]);
+      centroidBoundsMax[1] = std::max(centroidBoundsMax[1], primitiveInfoList[x].centroid[1]);
+      centroidBoundsMax[2] = std::max(centroidBoundsMax[2], primitiveInfoList[x].centroid[2]);
+    }
+    int dim = -1;
+    float diagonal[3] = {centroidBoundsMax[0] - centroidBoundsMin[0], centroidBoundsMax[1] - centroidBoundsMin[1], centroidBoundsMax[2] - centroidBoundsMin[2]};
+    if (diagonal[0] > diagonal[1] && diagonal[0] > diagonal[2]) {
+      dim = 0;
+    }
+    else {
+      if (diagonal[1] > diagonal[2]) {
+        dim = 1;
+      }
+      else {
+        dim = 2;
+      }
+    }
+
+    int mid = (start + end) / 2;
+    if (centroidBoundsMax[dim] == centroidBoundsMin[dim]) {
+      int firstPrimitiveOffset = orderedPrimitiveList.size();
+      for (int x = start; x < end; x++) {
+        orderedPrimitiveList.push_back(&primitiveInfoList[x]);
+      }
+
+      node->leftChild = NULL;
+      node->rightChild = NULL;
+
+      node->splitAxis = -1;
+      node->firstPrimitiveOffset = firstPrimitiveOffset;
+      node->primitiveCount = primitiveCount;
+    }
+    else {
+      std::nth_element(&primitiveInfoList[start], &primitiveInfoList[mid], &primitiveInfoList[end - 1] + 1, [dim](const PrimitiveInfo& a, const PrimitiveInfo& b) {
+        return a.centroid[dim] < b.centroid[dim];
+      });
+
+      node->splitAxis = dim;
+      node->firstPrimitiveOffset = -1;
+      node->primitiveCount = 0;
+
+      node->leftChild = recursiveBuild(primitiveInfoList, start, mid, totalNodes, orderedPrimitiveList);
+      node->rightChild = recursiveBuild(primitiveInfoList, mid, end, totalNodes, orderedPrimitiveList);
+    }
+  }
+
+  return node;
 }
--- src/acceleration_structure.h
+++ src/acceleration_structure.h
@@ -2,8 +2,33 @@
 
 #include "model.h"
 
+#include <algorithm>
+
+struct PrimitiveInfo {
+  int index;
+  float vertexA[3];
+  float vertexB[3];
+  float vertexC[3];
+
+  float boundsMin[3];
+  float boundsMax[3];
+  float centroid[3];
+};
+
+struct BVHBuildNode {
+  float boundsMin[3];
+  float boundsMax[3];
+  BVHBuildNode* leftChild;
+  BVHBuildNode* rightChild;
+
+  int splitAxis;
+  int firstPrimitiveOffset;
+  int primitiveCount;
+};
+
 class AccelerationStructure {
 private:
+  BVHBuildNode* recursiveBuild(std::vector<PrimitiveInfo>& primitiveInfoList, int start, int end, int* totalNodes, std::vector<PrimitiveInfo*>& orderedPrimitiveList);
 public:
   AccelerationStructure(Model* model);
   ~AccelerationStructure();
