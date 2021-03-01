edcbaeea23ed6c309c30b6fe89746de5f5f61129
WilliamLewww <WilliamMilesLew@gmail.com>
2021-02-28 18:05:17 -0800

    moved: render platform enum to structures header file

--- src/engine.h
+++ src/engine.h
@@ -6,12 +6,6 @@
 
 #include <string>
 
-enum RenderPlatform {
-  RENDER_PLATFORM_OPENCL,
-  RENDER_PLATFORM_CUDA,
-  RENDER_PLATFORM_VULKAN
-};
-
 class Engine {
 private:
   RenderPlatform renderPlatform;
