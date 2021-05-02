74a7656561ccc8061fd0475f5eb21fadfc1c0d65
William Lew <WilliamMilesLew@gmail.com>
2021-05-01 23:16:59 -0700

    Fixed linking OpenCL

--- lens_trace/include/renderer_optix.h
+++ lens_trace/include/renderer_optix.h
@@ -1,3 +1,5 @@
+#ifdef OPTIX_ENABLED
+
 #pragma once
 
 #include "renderer.h"
@@ -16,3 +18,5 @@ public:
 
   void render(void* pRenderProperties);
 };
+
+#endif
--- lens_trace/src/renderer_optix.cpp
+++ lens_trace/src/renderer_optix.cpp
@@ -1,3 +1,5 @@
+#ifdef OPTIX_ENABLED
+
 #include "renderer_optix.h"
 
 RendererOptiX::RendererOptiX() {
@@ -11,3 +13,5 @@ RendererOptiX::~RendererOptiX() {
 void RendererOptiX::render(void* pRenderProperties) {
   
 }
+
+#endif
